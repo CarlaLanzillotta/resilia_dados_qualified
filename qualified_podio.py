2f2e91a8da4c9b4b40df2bc90a9e52b74683025c
c.lanzillotta@hotmail.com <c.lanzillotta@hotmail.com>
2021-11-25 12:38:05 -0300

    primeiro commit

---
+++ qualified_podio.py
@@ -0,0 +1,22 @@
+def podio_olimpico(a,b,c):
+
+  
+  if (a<b) and (a<c):
+    if (b<c): 
+        return   f"1 - {a} minutos\n2 - {b} minutos\n3 - {c} minutos\n"
+    else:
+        return   f"1 - {a} minutos\n2 - {c} minutos\n3 - {b} minutos\n" 
+
+  if (b<a) and (b<c): 
+
+    if (a<c):
+      return   f"1 - {b} minutos\n2 - {a} minutos\n3 - {c} minutos\n"
+    else:
+      return  f"1 - {b} minutos\n2 - {c} minutos\n3 - {a} minutos\n"
+
+  else:
+    
+    if (a<b):
+      return   f"1 - {c} minutos\n2 - {a} minutos\n3 - {b} minutos\n"
+    else:
+      return   f"1 - {c} minutos\n2 - {b} minutos\n3 - {a} minutos\n"
