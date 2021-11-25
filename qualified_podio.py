596f0700d002f0e1f8e0962fa567ed084a9fcec6
c.lanzillotta@hotmail.com <c.lanzillotta@hotmail.com>
2021-11-25 12:44:52 -0300

    modificando arquivo

--- qualified_podio.py
+++ qualified_podio.py
@@ -20,3 +20,6 @@ def podio_olimpico(a,b,c):
       return   f"1 - {c} minutos\n2 - {a} minutos\n3 - {b} minutos\n"
     else:
       return   f"1 - {c} minutos\n2 - {b} minutos\n3 - {a} minutos\n"
+
+
+print('modificacao')
